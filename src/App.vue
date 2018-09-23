1fe2c9cf57c1d9346196f1dc15634bb8b70c88f1
unknown <geongeorge98@gmail.com>
2018-09-24 00:22:31 +0530

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>Testing out custom directives in <span v-highlight.blink="'#26cc60'">vue</span></p>
+    <p v-highlight="'red'">Hello</p>
+    <p v-highlight.blink="'green'">Hello</p>
   </div>
 </template>
 
@@ -23,6 +25,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  max-width: 400px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,22 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.directive("highlight", {
+  bind(el, binding, vnode) {
+    let ourcolor =  binding.value
+    
+    if(binding.modifiers['blink']){
+      let whitecolor = "white"
+      let color = ourcolor
+      setInterval(()=> {
+        color = color==ourcolor? whitecolor: ourcolor
+        el.style.background = color
+      },500)
+    } else {
+      el.style.background = ourcolor
+    }
+  }
+})
 new Vue({
   render: h => h(App)
 }).$mount('#app')
